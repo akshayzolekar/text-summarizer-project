3579cc3546ba41a7537fcb3a89b26542fda338ed
akshayzolekar <akshayzolekar@gmail.com>
2024-12-27 14:18:32 +0530

    app and prediction

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse, Response
+
+from textSummarizer.pipeline.prediction import PredictionPipeline
+
+text: str = "What is text summarization?"
+
+app = FastAPI()
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+@app.get("/train")
+async def training():
+    try:
+        os.system("python main.py")
+        return Response("Training successful")
+    except Exception as e:
+        return Response(f"Training failed: {str(e)}")
+
+
+@app.post("/predict")
+async def predict_route(text):
+    try:
+        obj = PredictionPipeline()
+        text = obj.predict(text)
+        return text
+    except Exception as e:
+        return e
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
---
+++ src/textSummarizer/pipeline/prediction.py
@@ -0,0 +1,22 @@
+from transformers import AutoTokenizer, pipeline
+
+from textSummarizer.config.configuration import ConfigurationManager
+
+
+class PredictionPipeline:
+    def __init__(self):
+        self.configc=ConfigurationManager().get_model_evaluation_config()
+
+    def predict(self, text):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
+        pipe = pipeline("summarization", model=self.config.model_path, tokenizer=tokenizer)
+
+        print("Dialogue:")
+        print(text)
+
+        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        print("\nModel Summary:")
+        print(output)
+
+        return output
